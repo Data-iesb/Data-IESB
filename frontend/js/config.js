6479331e6eeb83e3f970a0ad66b55736287f87d3
robertomdiniz <roberto.diniz@tisaude.com>
2025-06-02 18:38:56 -0300

    adjusting the host

--- frontend/js/config.js
+++ frontend/js/config.js
@@ -1,9 +1,9 @@
-// Produção
-// window.config = {
-//     API_URL : "http://ec2-3-82-1-192.compute-1.amazonaws.com/:80",
-// };
+Produção
+window.config = {
+    API_URL : "k8s-default-ingressi-73bd0705e3-102651203.sa-east-1.elb.amazonaws.com",
+};
 
 // Teste Local
-window.config = {
-    API_URL : "http://localhost:5000",
-};
+// window.config = {
+//     API_URL : "http://localhost:5000",
+// };
