6cc456b82532ca58ed4cfca14226795a29fa303e
robertomdiniz <roberto.diniz@tisaude.com>
2025-06-04 19:39:46 -0300

    debuging

--- frontend/js/config.js
+++ frontend/js/config.js
@@ -1,6 +1,6 @@
 // Produção
 window.config = {
-    API_URL : "k8s-default-ingressi-73bd0705e3-102651203.sa-east-1.elb.amazonaws.com",
+    API_URL : "http://k8s-default-ingressi-73bd0705e3-102651203.sa-east-1.elb.amazonaws.com",
 };
 
 // Teste Local
