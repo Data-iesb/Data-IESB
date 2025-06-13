6428364d90010d99f1077537e6c354dc10429cbc
robertomdiniz <roberto.diniz@tisaude.com>
2025-06-13 09:09:46 -0300

    adding the sas

--- frontend/js/config.js
+++
@@ -1,9 +0,0 @@
-// Produção
-window.config = {
-    API_URL : "http://k8s-default-ingressi-73bd0705e3-102651203.sa-east-1.elb.amazonaws.com",
-};
-
-// Teste Local
-//window.config = {
-//    API_URL : "http://localhost:5000",
-//};
